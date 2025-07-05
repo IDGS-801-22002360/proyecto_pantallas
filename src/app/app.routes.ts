b540f99a49396364595ea5be12f603a2447e82ba
marko <81442@alumnos.utleon.edu.mx>
2025-07-05 09:43:26 -0600

    actualizacion modulo clientes/prov

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
-import { PantallaPrincipal } from './pantalla-principal/pantalla-principal';
 import { Routes } from '@angular/router';
+import { PantallaPrincipal } from './pantalla-principal/pantalla-principal';
 import { CRUDEmpleados } from './crudempleados/crudempleados';
+//import { Clientes } from './clientes/clientes.ts';
 
 export const routes: Routes = [
   { path: 'inicio', component: PantallaPrincipal },
   { path: 'empleados', component: CRUDEmpleados },
+  //{ path: 'clientes', component:  Clientes},
   { path: '', redirectTo: 'empleados', pathMatch: 'full' }
 ];
 
